5490177327532f15457c90a4d7ac79ad987f1f9f
zzahraJ <r0883814@student.thomasmore.be>
2023-10-27 12:15:19 +0200

    feat: add environment

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
 // connect to mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/messagesApi");
+
+//console log .env MONGODB
+console.log(process.env.MONGODB);
 
 // check if connection works
 const db = mongoose.connection;
